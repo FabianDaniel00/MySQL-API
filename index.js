6f6faddbe79986d38df7b14b7af88ae1970d96f5
Fabian Daniel <kiloboggaming@gmail.com>
2021-03-13 14:02:56 +0100

    mysql pool

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-connection.connect((err) => {
+connection.getConnection((err) => {
   if (err) {
     console.log("\x1b[31m", err.message);
     return err.message;
